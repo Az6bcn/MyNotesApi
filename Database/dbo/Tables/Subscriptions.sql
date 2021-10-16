886032c8e6b9983bac2450ffdb01823f0e64d4d0
Azeez <az6bcn@gmail.com>
2021-10-17 00:08:01 +0100

    Added Database project to manage db changes

---
+++ Database/dbo/Tables/Subscriptions.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Subscriptions] (
+    [SubscriptionId] VARCHAR (50)       NOT NULL,
+    [Email]          VARCHAR (50)       NOT NULL,
+    [CustomerId]     VARCHAR (50)       NOT NULL,
+    [TrialExpired]   DATETIMEOFFSET (7) NOT NULL
+);
+
