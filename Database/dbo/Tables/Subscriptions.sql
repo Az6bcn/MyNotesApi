61cefd669e19035987dbc6a8fd6fc4455a64444f
Azeez <az6bcn@gmail.com>
2021-10-17 00:13:23 +0100

    db script minor changes

--- Database/dbo/Tables/Subscriptions.sql
+++ Database/dbo/Tables/Subscriptions.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Subscriptions] (
+    [Id]             INT                IDENTITY (1, 1) NOT NULL,
     [SubscriptionId] VARCHAR (50)       NOT NULL,
     [Email]          VARCHAR (50)       NOT NULL,
     [CustomerId]     VARCHAR (50)       NOT NULL,
-    [TrialExpired]   DATETIMEOFFSET (7) NOT NULL
+    [TrialExpired]   DATETIMEOFFSET (7) NOT NULL,
+    CONSTRAINT [PK_Subscriptions] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
