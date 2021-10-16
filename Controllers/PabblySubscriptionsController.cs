c68d0e3ae1b02fd895938943a3056c226ffb81f1
Azeez <az6bcn@gmail.com>
2021-10-16 23:52:13 +0100

    Read user with graph by their email in azb2c
    Read user with graph filtering the user id assigned to the user in azb2c

--- Controllers/PabblySubscriptionsController.cs
+++ Controllers/PabblySubscriptionsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.Graph;
-using Microsoft.Identity.Web.Resource;
 using MyNotesApi.MicrosoftGraphClient;
 using MyNotesApi.Models;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace MyNotesApi.Controllers
 {
@@ -20,25 +15,33 @@ namespace MyNotesApi.Controllers
     {
         private GraphClient _graphClient;
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AzureAdB2CSettings _azureAdB2CSettings;
 
         public PabblySubscriptionsController(GraphClient graphClient,
-                                             ILogger<WeatherForecastController> logger)
+                                             ILogger<WeatherForecastController> logger,
+                                             IOptions<AzureAdB2CSettings> options)
         {
             _graphClient = graphClient;
             _logger = logger;
+            _azureAdB2CSettings = options.Value;
         }
 
         [HttpPost("subscription-created")]
         public async Task<IActionResult> SubscriptionCreated([FromBody] JsonElement message)
         {
-            var dto = new WebHookSubscriptionCreatedDto();
-            dto.TryGetPropertyValues(message);
+            var webhookMessageDto = new WebHookSubscriptionCreatedDto();
+            webhookMessageDto.TryGetPropertyValues(message);
 
-            var users = await _graphClient.ServiceClient.Users.Request().GetAsync();
-            
-            //users.Where(x => x.FI)
-            
+            var filter = $"identities/any(id:id/Issuer eq '{_azureAdB2CSettings.TenantId}' and id/IssuerAssignedId eq '{webhookMessageDto.Email}')";
+            var user = await _graphClient.ServiceClient.Users.Request().Filter(filter).GetAsync();
+
+            // call graph to add subscriptionId and trialExpiryDate as attributes
+
+            // save to database
+
+
             return Ok();
         }
+
     }
 }
